d4aa8e5e486fb52591410f263e92d0cf1ae8c673
hontas <lundin.pontus@gmail.com>
2017-11-20 01:08:14 +0100

    babelify and uglify dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
 export default {
   input: 'index.mjs',
   output: {
     file: 'dist/arnold-says.js',
     format: 'umd',
     name: 'arnold-says'
-  }
+  },
+  plugins: [
+    babel({
+      presets: [ 'es2015-rollup' ],
+      babelrc: false,
+      comments: true,
+      runtimeHelpers: true
+    }),
+    uglify()
+  ]
 };
